bf76982d10c7060918148b7f5523ce4ef3887df2
thanhtruong <thanhlinhna182025@gmail.com>
2022-10-31 13:05:16 +0700

    Create sidebar

---
+++ src/layouts/components/Sidebar/Sidebar.js
@@ -0,0 +1,32 @@
+import classNames from 'classnames/bind';
+import styles from './Sidebar.module.scss';
+import Menu, { MenuItem } from './Menu';
+import config from '~/configs';
+import {
+  HomeIcon,
+  HomeFullFillIcon,
+  UsersIcon,
+  UsersFullFillIcon,
+  LiveIcon,
+  LiveFullFillIcon,
+} from '~/components/Icons';
+
+const cx = classNames.bind(styles);
+function Sidebar() {
+  return (
+    <aside className={cx('wrapper')}>
+      <Menu>
+        <MenuItem to={config.routes.home} title="For You" icon={<HomeIcon />} activeIcon={<HomeFullFillIcon />} />
+        <MenuItem
+          to={config.routes.following}
+          title="Following"
+          icon={<UsersIcon />}
+          activeIcon={<UsersFullFillIcon />}
+        />
+        <MenuItem to={config.routes.live} title="LIVE" icon={<LiveIcon />} activeIcon={<LiveFullFillIcon />} />
+      </Menu>
+    </aside>
+  );
+}
+
+export default Sidebar;
